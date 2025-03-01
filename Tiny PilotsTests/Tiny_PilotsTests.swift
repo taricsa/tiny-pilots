d42d9b4cd31f3ba0aad101300a775803484ffa35
Taric <taric.andrade@gmail.com>
2025-03-01 08:27:02 -0800

    Initial Commit

---
+++ Tiny PilotsTests/Tiny_PilotsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tiny_PilotsTests.swift
+//  Tiny PilotsTests
+//
+//  Created by Taric Santos de Andrade on 2025-03-01.
+//
+
+import Testing
+@testable import Tiny_Pilots
+
+struct Tiny_PilotsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Tiny PilotsUITests/Tiny_PilotsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Tiny_PilotsUITestsLaunchTests.swift
+//  Tiny PilotsUITests
+//
+//  Created by Taric Santos de Andrade on 2025-03-01.
+//
+
+import XCTest
+
+final class Tiny_PilotsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
